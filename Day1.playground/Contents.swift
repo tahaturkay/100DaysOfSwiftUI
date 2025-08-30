b59077a8bb36561753ed38d6ca021f493239f80e
Taha Turkay <tahaturkay01@gmail.com>
2025-08-30 23:46:26 +0300

    Day1 added

---
+++ Day1.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+var greeting = "Hello, playground"
+greeting = "Selamlar"
+print(greeting)
+greeting = "Saygılar"
+print(greeting)
+
+
+let surname = "aktaş"
+let conventionWord = "this is the way among the swift developers le Camel"
+let managerName = "taha"
