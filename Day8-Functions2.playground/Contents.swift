afd366de6afbad78dc98aab1695c4450a938e4b7
Taha Turkay <tahaturkay01@gmail.com>
2025-10-27 16:27:03 +0200

    Day8 Started

---
+++ Day8-Functions2.playground/Contents.swift
@@ -0,0 +1,47 @@
+// After defining parameters type if we equal this value to some initial value, it will be default value for this parameter.
+func rollingDice(surface: Int, for count: Int=3) -> [Int]{
+    var rolledStat = [Int]()
+    for _ in 1...count{
+        var rolled = Int.random(in: 1...surface)
+        rolledStat.append(rolled)
+    }
+    return rolledStat
+}
+rollingDice(surface: 8, for: 5)
+// If function doesn't have any parameter fpr "for" it will automatically take this value as default value which is "3".
+rollingDice(surface: 6)
+
+// By calling "keepingCapacity" removeAll will remove all the elements in the "members" but the capacity size will be keep available for future addition into the array. If we wouldn't wrote this the memory also would be released as suppoused to be.
+// So we can say that the parameter of keepingCapacity is also has default value "false" because we can use this function without writing "keepingCapacity:false".
+var members = ["Taha","Zülal"]
+members.removeAll(keepingCapacity: true)
+
+// Handling errors
+// Error is existing type.
+enum PasswordError: Error{
+    case short,obvious
+}
+let password = PasswordError.short
+print(type(of: password))
+
+// If "error" will be thrown in the function before assigning return type we write "throws". In function code block first we write throw conditions.
+func passwordChecker(_ userPassword: String) throws -> String{
+    if userPassword.count < 5{
+        throw PasswordError.short
+    }
+    if userPassword == "12345" || userPassword == "password"{
+        throw PasswordError.obvious
+    }
+    if userPassword.count < 8{
+        return "Weak but OK"
+    }
+    else if userPassword.count < 10{
+        return "Good password"
+    }
+    else{
+        return "Excellent password!"
+    }
+}
+// After error throwed the rest of the code won't work.
+//try passwordChecker("12345")
+try print(passwordChecker("taha1234"))
