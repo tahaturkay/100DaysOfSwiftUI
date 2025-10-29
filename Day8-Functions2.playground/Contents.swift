68d279d12aa14267c4b597479da91f0e5ff8431c
Taha Turkay <tahaturkay01@gmail.com>
2025-10-29 22:13:54 +0200

    Day8-Checkpoint-4

--- Day8-Functions2.playground/Contents.swift
+++ Day8-Functions2.playground/Contents.swift
@@ -70,3 +70,11 @@ do {
 } catch {
     print("There was an error.")
 }
+
+// ##### Checkpoint - 4 #####
+
+//write a function that accepts an integer from 1 through 10,000, and returns the integer square root of that number. That sounds easy, but there are some catches:
+//You can’t use Swift’s built-in sqrt() function or similar – you need to find the square root yourself.
+//If the number is less than 1 or greater than 10,000 you should throw an “out of bounds” error.
+//You should only consider integer square roots – don’t worry about the square root of 3 being 1.732, for example.
+//If you can’t find the square root, throw a “no root” error.
