38247474316fafc6878b2aba46f64292831d6ba6
Taha Turkay <tahaturkay01@gmail.com>
2025-11-03 16:29:00 +0200

    Day9
    Closures are finished but I need to study more on this topic. Then I will complete the checkpoint task

--- Day9-Closures.playground/Contents.swift
+++ Day9-Closures.playground/Contents.swift
@@ -53,6 +53,7 @@ let captainFirstTeam = team.sorted(by: captainFirstSorted)
 print(captainFirstTeam)
 
 // We can directliy customize the sort function with closures
+// Rule is it get two element from array and return a boolean value
 let captainFirstTeam2 = team.sorted(by: { (name1: String, name2: String) -> Bool in
     if name1 == "Suzanne" {
         return true
@@ -62,3 +63,53 @@ let captainFirstTeam2 = team.sorted(by: { (name1: String, name2: String) -> Bool
 
     return name1 < name2
 })
+
+// Also we don't have to write parameter names because swift automatically assign this paramters according to sort function. So, by calling $0 and $1 these elements represents the index of array.
+let capraintFirstTeam3 = team.sorted{
+    if $0 == "Suzanne"{
+        return true
+    }
+    else if $1 == "Suzanne"{
+        return false
+    }
+    else{
+        return $0 < $1
+    }
+}
+// In order to be fast we can use that indexes for example if we want to sort elements reversed we can do these;
+let reversedSort = team.sorted(){
+    return $0 > $1
+}
+print(reversedSort)
+
+// So $0 element is traversal the array as I understand.
+let onlyT = team.filter{ ($0.hasPrefix("T"))}
+print(onlyT)
+
+let upperCasedTeam = team.map{ ($0.uppercased())}
+print(upperCasedTeam)
+
+
+func generator() -> Double{
+    Double.random(in: 1...100)
+}
+
+// We can use functions as a parameter for the new functions
+func makeArray(size: Int, using generator: ()-> Double ) -> [Double]{
+    var newArray = [Double] ()
+    for _ in 0...size{
+        var newNumber = generator()
+        newArray.append(newNumber)
+    }
+    return newArray
+}
+print(makeArray(size: 10, using: generator))
+
+// So, just defining size of makeArray we automatically fill the generator and use it for the rolls function
+let rolls = makeArray(size: 50) {
+    Double.random(in: 1...20)
+}
+
+print(rolls)
+
+// Closures is very important for SwiftUI. I need to make exercises for that topic!!!
